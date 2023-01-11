b8fd37c9cd101b9219fe6845d6eb277578cba148
Ronit Mukherjee <Ronit.Mukherjee@lntinfotech.com>
2023-01-11 11:44:34 +0000

    Added Script.ps1 to /

---
+++ Script.ps1
@@ -0,0 +1,45 @@
+param (
+[string]$WebSiteName,
+[string]$WebSitePath,
+[string]$Port,
+[string]$Protocol,
+[string]$AppPool
+)
+if(-not (Get-Module WebAdministration)){
+    Import-Module WebAdministration
+}
+$IsNewSite = $False
+$WebSiteDetails= Get-ChildItem -Path IIS:\Sites
+$IISAppPool = Get-ChildItem –Path IIS:\AppPools 
+Write-Output '**********Starting Website Deployment in IIS**********'
+#Write-Output $WebSiteDetails.Name
+foreach($siteName in $WebSiteDetails.Name){
+  if($siteName -eq $WebSiteName){
+     $IsNewSite=$False
+     break
+   }else{
+     $IsNewSite=$true
+   }
+}
+if($IsNewSite){
+   $IspoolExists=$False
+   Write-Output '************ New Site************'
+   foreach($Pool in $IISAppPool){
+     if($Pool.Name -eq '.NET v4.5'){ 
+       #You can update the Pool name with variable declared at top and pass it from TFS Release Management
+       $IspoolExists=$true
+      }
+   }
+   if(-not $IspoolExists){
+     #New-Item IIS:\AppPools\$AppPool
+     Write-Output '************Pool Not Available************'
+  }else{
+    Write-Output '************Pool Exists************'
+    New-Item IIS:\Sites\$WebSiteName -physicalPath $WebSitePath -bindings @{protocol= $Protocol ;bindingInformation=$Port}
+    Set-ItemProperty IIS:\Sites\$WebSiteName -name applicationPool -value '.NET v4.5' #You can update the Pool name with variable declared at top and pass it from TFS Release Management
+  }
+
+}else{
+    Write-Output '************ Site Already Exists ************'
+}
+Write-Output '**********End Website Deployment in IIS**********'
