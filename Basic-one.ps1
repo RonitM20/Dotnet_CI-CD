d59527ee24c22aba4c001aec425f97082b9a1cdf
Ronit Mukherjee <Ronit.Mukherjee@lntinfotech.com>
2023-01-16 11:17:18 +0000

    Updated Basic-one.ps1

--- Basic-one.ps1
+++ Basic-one.ps1
@@ -6,11 +6,8 @@ param(
 
 Import-Module WebAdministration
 
-# Create a new website
 New-Website -Name $websiteName -Port 80 -PhysicalPath $physicalPath
 
-# Copy the build artifact to the website's physical path
 Copy-Item "$buildArtifactPath\*" -Destination $physicalPath -Recurse
 
-# Start the website
 Start-Website -Name $websiteName
