9b94d06dc8917d5ce042aa798f25f027fda91a6c
Ronit Mukherjee <Ronit.Mukherjee@lntinfotech.com>
2023-01-16 11:14:05 +0000

    Updated Main-one.ps1

--- Main-one.ps1
+++ Main-one.ps1
@@ -5,15 +5,9 @@ param (
 [string]$Protocol
 )
 
- 
-
 Import-Module WebAdministration
 
- 
-
-copy-item "$(System.DefaultWorkingDirectory)/_Ron_devops (1)/Azurebuildpipeline/Helloworldapp.web.zip" -Destination $PhysicalPath -Recurse
-
- 
+#copy-item "$(System.DefaultWorkingDirectory)/_Ron_devops (1)/Azurebuildpipeline/Helloworldapp.web.zip" -Destination $PhysicalPath -Recurse
 
 # Checking if website already there
 if (-not(Test-Path IIS:\Sites\$WebsiteName)) {
@@ -21,19 +15,13 @@ if (-not(Test-Path IIS:\Sites\$WebsiteName)) {
 New-Item IIS:\Sites\$WebsiteName -bindings @{protocol=$Protocol;bindingInformation=":80:"} -physicalPath $PhysicalPath
 }else {Write-Output 'Site already there'}
 
- 
-
 # Checking if app pool already there
 if (-not(Test-Path IIS:\AppPools\$AppPoolName)) {
 # Create the app pool
 New-Item IIS:\AppPools\$AppPoolName
 }
 
- 
-
 # Assign the app pool to the website
 Set-ItemProperty IIS:\Sites\$WebsiteName -name applicationPool -value $AppPoolName
-
- 
 
 Write-Output 'End of site deployment'
