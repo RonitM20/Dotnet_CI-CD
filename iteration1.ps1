55ea93fea42099078807b5fa0f9eb3c6b24dc55f
Ronit Mukherjee <ronit.mukherjee@lntinfotech.com>
2023-02-05 20:11:19 +0530

    testing git

---
+++ iteration1.ps1
@@ -0,0 +1,15 @@
+﻿$all_RGs=(Get-AzResourceGroup).resourcegroupname
+$rgname= "test" 
+$ron=(get-azresourcegroup | where{$_.ResourceGroupName -match $rgname}).resourcegroupname  
+foreach ($r in $ron) {
+#$last3=@()
+$last3=($r -replace '\D+(\d+)','$1') -split "00",-2 | Sort-Object
+[array]$last3
+}
+$test= $last3 | measure -Maximum  
+$ram=$test.Maximum
+$rom= ++$ram
+
+$final= $rgname + $rom
+
+Write-Output $final
