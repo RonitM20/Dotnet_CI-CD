47935fe7270347595707c3520bf5f5221fd3ba85
RonitM20 <110469725+RonitM20@users.noreply.github.com>
2024-05-29 18:59:59 +0530

    Update iteration2.ps1

--- iteration2.ps1
+++ iteration2.ps1
@@ -1,9 +1,1 @@
-﻿$rgname= "test" 
-$ron=(get-azresourcegroup | where{$_.ResourceGroupName -match $rgname}).resourcegroupname
-$div = ($ron -replace '\D+(\d+)','$1') -split "00",-2 | Sort-Object
-$div = $div |  Select -Last 1
-$div = [int]$div + 1
-$a = $ron | Select -Last 1
-$b = $a.Substring(0,4)
-$c = $b + $div
-Write-Output $c
+write-output "${{secrets.test_secret}}
